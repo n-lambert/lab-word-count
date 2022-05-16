42c2d84522a859fbc876b4780b8782986cbc5e9d
Nicole Lambert <nicole.rose.lambert9@gmail.com>
2022-05-16 14:24:44 -0500

    completed wordcount.py

--- wordcount.py
+++ wordcount.py
@@ -1,4 +1,20 @@
 """Count words in file."""
+def word_count(data_file):
 
- 
-# put your code here.
+    input_file = open(data_file)
+    #opens file
+
+    word_frequency = {}
+
+    for line in input_file:
+        line = line.rstrip() 
+        words = line.split(" ")
+        for word in words:
+            word_frequency[word] = word_frequency.get(word,0) + 1
+
+    for word, frequency in word_frequency.items():
+        print(f'{word} {frequency}')
+    # print(word_frequency.items())
+
+
+word_count("twain.txt")
